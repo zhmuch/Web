19e90e9fc2ed9b346fd135ed2174f146c1a97026
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-10-05 01:40:11 -0400

    Create script.js

---
+++ HTML-Javascript-CSS/11/script.js
@@ -0,0 +1,6 @@
+function processTravelForm() {
+    var firstName = document.myTravelForm.firstname.value;
+    //add lastname and destination variables here
+    var lastName = document.myTravelForm.lastname.value;
+    var destination = document.myTravelForm.destination.value;
+}
