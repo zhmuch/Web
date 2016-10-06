d38dd0f21a4ca19065120d519a2566a71b80d8a6
Muchen Zhang <zhmuch@users.noreply.github.com>
2016-10-05 21:00:06 -0400

    Return result in resultField

---
+++ HTML-Javascript-CSS/18/script.js
@@ -0,0 +1,25 @@
+function processForm() {
+    
+    var temperature = Number(document.tempForm.temp.value);
+    var tempType;
+    var result;
+
+    for (var i=0; i < document.tempForm.choice.length; i++) {
+        
+        if (document.tempForm.choice[i].checked) {
+            tempType = document.tempForm.choice[i].value;
+        }
+    }
+
+    if (tempType == 'fahrenheit') {
+        result = temperature * 9/5 + 32;
+    }
+    
+    else {
+        result = (temperature -  32)  *  5/9;
+    }
+    
+    // Assign the result field value here
+    document.tempForm.resultField.value = result;
+    
+}
